e7bbcee3789e629ffc4a1a9e2ee5902cc5c2a2a3
Baskar7499 <baskar.i@logikoof.in>
2024-04-17 21:16:33 +0530

    mini website

--- HTML_Day_5_Task_1/day5.js
+++ HTML_Day_5_Task_1/day5.js
@@ -1,5 +1,5 @@
-let a = "Hello";
+// let a = "Hello";
 
-let creatHeaderElements = document.querySelector("h2");
-creatHeaderElements.innerText = a;
-document.body.appendChild(creatHeaderElements);
+// let creatHeaderElements = document.querySelector("h2");
+// creatHeaderElements.innerText = a;
+// document.body.appendChild(creatHeaderElements);
